d469da25151621568becd0af3ebf01a7aa73b59d
unknown <kirschjanosmark@gmail.com>
2019-03-13 17:51:37 +0100

    VisualStudio frissítése előtti állapot

--- Labirintus/Assets/JatekosMozgas.cs
+++ Labirintus/Assets/JatekosMozgas.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class JatekosMozgas : MonoBehaviour
 {
+
+    public Rigidbody jatekosTest;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        jatekosTest.AddForce();
     }
 
     // Update is called once per frame
