65208c6173aaf3a6eee9e9cd40721fbf253c9460
unknown <kirschjanosmark@gmail.com>
2019-03-13 20:35:48 +0100

    Kamerakövetés, minden patent!

---
+++ Labirintus/Assets/kameraKovetes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class kameraKovetes : MonoBehaviour
+{
+    public Transform jatekosTranszformacio;
+    public Vector3 eltolas;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = jatekosTranszformacio.position + eltolas;
+    }
+}
