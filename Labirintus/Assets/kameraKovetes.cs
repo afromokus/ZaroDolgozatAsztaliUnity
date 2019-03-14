4c3c7ff40a7450db0ea1c2cf0d27d7756ee3a4f5
wot14 <wot14@citromail.hu>
2019-03-14 11:43:57 +0100

    Forgás megfagyasztva, falak érzékelése a kamerán

--- Labirintus/Assets/kameraKovetes.cs
+++ Labirintus/Assets/kameraKovetes.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 public class kameraKovetes : MonoBehaviour
 {
     public Transform jatekosTranszformacio;
+    public Rigidbody kameraTest;
     public Vector3 eltolas;
 
+    public Vector3 nullVector;
+
+    private void Start()
+    {
+        eltolas.x = 5;
+        eltolas.y = 1.5f;
+        eltolas.z = 0;
+
+        nullVector = new Vector3(0, 0, 0);
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = jatekosTranszformacio.position + eltolas;
+    }
+
+    private void OnCollisionEnter(Collision utkozesKamera)
+    {
+        if (utkozesKamera.collider.tag == "Fal")
+        {
+            kameraTest.freezeRotation = true;
+        }
     }
 }
