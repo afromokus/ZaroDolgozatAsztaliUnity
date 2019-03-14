2b0d9e5eb9e9797d169dde04d2699360587c6e1e
wot14 <wot14@citromail.hu>
2019-03-14 10:49:01 +0100

    Tagelés, kameraütközés

---
+++ Labirintus/Assets/KameraUtkozes.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class KameraUtkozes : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision utkozesInfo)
+    {
+        if (utkozesInfo.collider.name.Contains("Fal"))
+        {
+            Debug.Log("Ütközés fallal!");
+        }
+    }
+
+}
