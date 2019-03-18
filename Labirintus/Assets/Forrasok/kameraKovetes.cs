a79cd23e5049ad9ad85387d51a3bfc03caf3a04f
wot14 <wot14@citromail.hu>
2019-03-18 13:19:34 +0100

    Raycasting

---
+++ Labirintus/Assets/Forrasok/kameraKovetes.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class kameraKovetes : MonoBehaviour
+{
+    public Transform jatekosTranszformacio;
+    public Rigidbody kameraTest;
+    public Vector3 Eltolas;
+    Ray tolatasFigyeles;
+    RaycastHit elkerulObj;
+    enum Pozicio { alap, kozeli, felul }
+    Pozicio kameraAllapot;
+    Vector3 figyeloVektor;
+
+    private void Start()
+    {
+        kameraTest.freezeRotation = true;
+
+        Eltolas = new Vector3(5, 1.5f, 0);
+        figyeloVektor = new Vector3(5, 0, 0);
+
+        Cursor.visible = false;
+
+        tolatasFigyeles = new Ray(transform.position, figyeloVektor);
+        kameraAllapot = Pozicio.alap;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = jatekosTranszformacio.position + Eltolas;
+        tolatasFigyeles.origin = kameraTest.position;
+
+
+
+        Debug.DrawLine(tolatasFigyeles.origin, tolatasFigyeles.origin + figyeloVektor, Color.red);
+
+    }
+
+}
