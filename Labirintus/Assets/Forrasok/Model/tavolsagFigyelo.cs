a7d00e99849e17e5bba1b6d5202d707453280f39
unknown <kirschjanosmark@gmail.com>
2019-03-18 16:48:57 +0100

    Távolságfigyelő osztály elkészítve, megfelelően működik.

---
+++ Labirintus/Assets/Forrasok/Model/tavolsagFigyelo.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Model
+{
+    class TavolsagFigyelo
+    {
+        Ray sugar;
+
+        Vector3 sugarHossz;
+
+        public TavolsagFigyelo(Ray sugar)
+        {
+            this.sugar = sugar;
+            this.sugarHossz = sugar.origin;
+        }
+
+        public TavolsagFigyelo(Vector3 kezdoPont, Vector3 eltolas)
+        {
+            this.sugar = new Ray(kezdoPont, eltolas);
+            this.sugarHossz = eltolas;
+        }
+
+        public Ray getSugar()
+        {
+            return sugar;
+        }
+
+        public void setSugar(Ray sugar)
+        {
+            this.sugar = sugar;
+        }
+
+        public void setSugarOrigin(Vector3 origin)
+        {
+            sugar.origin = origin;
+        }
+
+        public bool utkozikE()
+        {
+            return Physics.Raycast(sugar, sugarHossz.x);
+        }
+
+        public void setSugarHossz(float x)
+        {
+            sugarHossz.Set(x, 0, 0);
+        }
+
+        public void rajzolFigyelo()
+        {
+            Debug.DrawLine(sugar.origin, sugar.origin + sugarHossz);
+        }
+
+    }
+}
--- Labirintus/Assets/Forrasok/kameraKovetes.cs
+++ Labirintus/Assets/Forrasok/kameraKovetes.cs
@@ -1,27 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using Assets.Model;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class kameraKovetes : MonoBehaviour
 {
     public Transform jatekosTranszformacio;
-    public Rigidbody kameraTest;
+    public Transform kameraTest;
     public Vector3 Eltolas;
-    Ray tolatasFigyeles;
-    RaycastHit elkerulObj;
     enum Pozicio { alap, kozeli, felul }
     Pozicio kameraAllapot;
-    Vector3 figyeloVektor;
+    Vector3 figyeloVektorHatra;
+
+    TavolsagFigyelo tolatasFigyeles;
+
+    float rovidVektorhossz = 0.5f;
+    float hosszuVektorhossz;
 
     private void Start()
     {
-        kameraTest.freezeRotation = true;
-
         Eltolas = new Vector3(5, 1.5f, 0);
-        figyeloVektor = new Vector3(0.2f, 0, 0);
+        hosszuVektorhossz = Eltolas.x;
+        figyeloVektorHatra = new Vector3(rovidVektorhossz, 0, 0);
 
         Cursor.visible = false;
 
-        tolatasFigyeles = new Ray(transform.position, figyeloVektor);
+        tolatasFigyeles = new TavolsagFigyelo(transform.position, figyeloVektorHatra);
         kameraAllapot = Pozicio.alap;
 
     }
@@ -30,25 +34,33 @@ public class kameraKovetes : MonoBehaviour
     void Update()
     {
         transform.position = jatekosTranszformacio.position + Eltolas;
-        tolatasFigyeles.origin = kameraTest.position;
+        tolatasFigyeles.setSugarOrigin(kameraTest.position);
 
-        if (Physics.Raycast(tolatasFigyeles, 0.2f) && kameraAllapot == Pozicio.alap)
+        if (kameraAllapot == Pozicio.alap && tolatasFigyeles.utkozikE())
         {
-            Debug.Log("Nekiment!");
-            Eltolas.x = 2f;
-            figyeloVektor.x = 5f;
-            kameraAllapot = Pozicio.kozeli;
+            kameraValtAlapPoz();
         }
-        else if (!Physics.Raycast(tolatasFigyeles, 5f) && kameraAllapot == Pozicio.kozeli)
+        else if (kameraAllapot == Pozicio.kozeli && !tolatasFigyeles.utkozikE())
         {
-            Debug.Log("Kijöhet!");
-            Eltolas.x = 5f;
-            figyeloVektor.x = 0.2f;
-            kameraAllapot = Pozicio.alap;
+            kameraValtKozeli();
         }
 
-        Debug.DrawLine(tolatasFigyeles.origin, tolatasFigyeles.origin + figyeloVektor, Color.red);
+        tolatasFigyeles.rajzolFigyelo();
 
+    }
+
+    void kameraValtAlapPoz()
+    {
+        Eltolas.x = 2f;
+        tolatasFigyeles.setSugarHossz(hosszuVektorhossz);
+        kameraAllapot = Pozicio.kozeli;
+    }
+
+    void kameraValtKozeli()
+    {
+        Eltolas.x = 5f;
+        tolatasFigyeles.setSugarHossz(rovidVektorhossz);
+        kameraAllapot = Pozicio.alap;
     }
 
 }
