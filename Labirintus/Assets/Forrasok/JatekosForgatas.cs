b0fb4e87e6bee522bfc94ee52ba5dafbb8941a43
wot14 <wot14@citromail.hu>
2019-03-19 13:38:50 +0100

    Játékos forog

---
+++ Labirintus/Assets/Forrasok/JatekosForgatas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JatekosForgatas : MonoBehaviour {
+
+    float forgatasiSebesseg = 5f;
+    Vector3 forgatasVektor;
+
+	// Use this for initialization
+	void Start () {
+        forgatasVektor = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        forgatasVektor.y += forgatasiSebesseg * Input.GetAxis("Mouse X");
+
+        transform.eulerAngles = forgatasVektor;
+
+	}
+}
