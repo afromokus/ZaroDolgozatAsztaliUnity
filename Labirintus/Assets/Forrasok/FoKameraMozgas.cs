d57dc8e98dc582e3c5958f0b4a90dbaa1ba6deeb
unknown <kirschjanosmark@gmail.com>
2019-03-21 11:54:31 +0100

    Kamaranézet váltás közeli és alap között y tengelyen működő

--- Labirintus/Assets/Forrasok/FoKameraMozgas.cs
+++ Labirintus/Assets/Forrasok/FoKameraMozgas.cs
@@ -4,73 +4,86 @@ using Assets.Model;
 
 public class FoKameraMozgas : MonoBehaviour
 {
-    Vector3 eltolasAlap;
+    public Transform foKameraTransform;
+
     Vector3 eltolasKozeli;
     Vector3 eltolasFelul;
-    enum Pozicio { alap, kozeli, felul}
+    enum Pozicio { alap, kozeli, felul }
 
     Pozicio kameraAllapot;
 
     VektorSugar figyeloKameraEloreX;
+    VektorSugar kijovetelFigyelo;
     VektorSugar balraFigyelo;
-
-    Vector3 forgatasNormal;
-    Vector3 forgatasLefele;
 
     Vector3 balraFigyeloEltolas;
 
     float tolatasRadarVisszah = -1f;
     float rovidVektorHossz = 0.5f;
-
-
+    private Vector3 eltolasAlap;
 
     private void Start()
     {
         eltolasAlap = new Vector3(5f, 1.5f, 0f);
-        eltolasKozeli = new Vector3(-3f, 0f, 0f);
+
+        eltolasKozeli = new Vector3(2f, 1.5f, 0f);
         eltolasFelul = new Vector3(-2f, 1.5f, 0f);
 
-        balraFigyeloEltolas = new Vector3(-0.5f,0,0);
+        balraFigyeloEltolas = new Vector3(-0.5f, 0, 0);
 
-        forgatasNormal = new Vector3(0f, -90f, 0f);
-        forgatasLefele = new Vector3(80f, -90f, 0f);
-
+        valtKameraAlaphelyzetbe();
         kameraAllapot = Pozicio.alap;
 
         figyeloKameraEloreX = new VektorSugar(transform.position, rovidVektorHossz + tolatasRadarVisszah);
+        kijovetelFigyelo = new VektorSugar(transform.position, 5f);
         balraFigyelo = new VektorSugar(transform.position, new Vector3(0f, 0f, 2f));
-        
+
         transform.position = transform.parent.position + eltolasAlap;
-        transform.eulerAngles = forgatasNormal;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
         figyeloKameraEloreX.setSugarOrigin(transform.position);
+        kijovetelFigyelo.setSugarOrigin(transform.position);
         balraFigyelo.setSugarOrigin(transform.position + balraFigyeloEltolas);
 
-        if (figyeloKameraEloreX.utkozikEX())
+        if (kameraAllapot == Pozicio.kozeli && !kijovetelFigyelo.utkozikEX())
         {
-            Debug.Log("ütközik");
+            valtKameraAlaphelyzetbe();
+            kameraAllapot = Pozicio.alap;
         }
-
-        if (kameraAllapot == Pozicio.alap && figyeloKameraEloreX.utkozikEX())
+        else if (kameraAllapot == Pozicio.alap && figyeloKameraEloreX.utkozikEX())
         {
-            transform.position = transform.position + eltolasKozeli;
-            transform.eulerAngles = forgatasNormal;
+            valtKameraKozeliNezet();
             kameraAllapot = Pozicio.kozeli;
         }
         else if (kameraAllapot == Pozicio.kozeli && figyeloKameraEloreX.utkozikEX())
         {
             transform.position = transform.position + eltolasFelul;
-            transform.eulerAngles = forgatasLefele;
+            transform.LookAt(transform.parent);
             kameraAllapot = Pozicio.felul;
         }
 
+
         figyeloKameraEloreX.rajzolFigyelo(Color.white);
+        kijovetelFigyelo.rajzolFigyelo();
         balraFigyelo.rajzolFigyelo(Color.red);
 
     }
+
+    void valtKameraAlaphelyzetbe()
+    {
+        transform.localPosition = eltolasAlap;
+        transform.LookAt(transform.parent);
+    }
+
+    void valtKameraKozeliNezet()
+    {
+        transform.localPosition = eltolasKozeli;
+        transform.LookAt(transform.parent);
+    }
+
 }
